10d2fc43c131102c3af86233d439e320a756f749
Ruth shtraicher <RutProgramming8978@gmail.com>
2025-11-02 12:33:53 +0200

    add schema

--- .next/dev/static/chunks/pages/_error.js
+++ .next/dev/static/chunks/pages/_error.js
@@ -1,15 +1,15 @@
 __turbopack_load_page_chunks__("/_error", [
-  "static/chunks/node_modules_next_dist_compiled_c629999b._.js",
-  "static/chunks/node_modules_next_dist_shared_lib_cf5b50a6._.js",
-  "static/chunks/node_modules_next_dist_client_d0aa886c._.js",
-  "static/chunks/node_modules_next_dist_19fd0646._.js",
-  "static/chunks/node_modules_next_error_1cfbb379.js",
-  "static/chunks/[next]_entry_page-loader_ts_43b523b5._.js",
-  "static/chunks/node_modules_react_b4bd21af._.js",
-  "static/chunks/node_modules_react-dom_cjs_react-dom_development_2b5e0eb3.js",
-  "static/chunks/node_modules_react-dom_8a8085df._.js",
-  "static/chunks/node_modules_416396d1._.js",
-  "static/chunks/[root-of-the-server]__092393de._.js",
-  "static/chunks/pages__error_2da965e7._.js",
-  "static/chunks/turbopack-pages__error_a5919942._.js"
+  "static/chunks/278b9_next_dist_compiled_8a5631f8._.js",
+  "static/chunks/278b9_next_dist_shared_lib_991ed804._.js",
+  "static/chunks/278b9_next_dist_client_dc5ed11f._.js",
+  "static/chunks/278b9_next_dist_1ce60097._.js",
+  "static/chunks/278b9_next_error_da4eecb1.js",
+  "static/chunks/[next]_entry_page-loader_ts_08b438b0._.js",
+  "static/chunks/278b9_react_2227ef40._.js",
+  "static/chunks/278b9_react-dom_cjs_react-dom_development_ad2903f9.js",
+  "static/chunks/278b9_react-dom_20d5103b._.js",
+  "static/chunks/278b9_38561b51._.js",
+  "static/chunks/[root-of-the-server]__a6824cfd._.js",
+  "static/chunks/c4949_all lessons_Project-team_community-lending-platform_pages__error_2da965e7._.js",
+  "static/chunks/c5fd8_all lessons_Project-team_community-lending-platform_pages__error_28b1fa90._.js"
 ])
